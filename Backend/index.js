dfd7018a04354cdfb7bacd197140ad8a422db89a
harsha12a <harsha.pss2005@gmail.com>
2025-03-02 11:02:47 +0530

    complete:core funtions#2

--- Backend/index.js
+++ Backend/index.js
@@ -17,15 +17,6 @@ app.get('/', (req, res) => {
     res.send('🚀 TeamTrek API is running...')
 })
 
-
-// async function getTasksForUser(userId) {
-//   const tasks = await Task.find({ assignedTo: userId }).populate("assignedTo").exec();
-//   console.log(tasks);
-// }
-
-// getTasksForUser("65d4f0c7a8b3d5f2c1e9b012");
-
-
 app.listen(PORT, () => {
     console.log(`⚡ Server running on port ${PORT}`)
 })
